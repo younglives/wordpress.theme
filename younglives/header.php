08d25f696ad9afa7cad3854d0256585114158eb6
Michael Davis <m.r.davis@me.com>
2013-07-14 18:42:42 +0000

    Remove the text in the logo

--- younglives/header.php
+++ younglives/header.php
@@ -48,11 +48,7 @@
 				<div id="inner-header" class="wrap clearfix">
 
 					<!-- to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> -->
-					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
-
-					<!-- if you'd like to use the site description you can un-comment it below -->
-					<?php // bloginfo('description'); ?>
-
+					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo get_template_directory_uri(); ?>/library/images/nothing.gif" /></a></p>
 
 					<nav role="navigation">
 						<?php bones_main_nav(); ?>
