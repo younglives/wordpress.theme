d67363691528874d4a334b600d9fb19be590050b
Michael Davis <m.r.davis@me.com>
2013-07-14 18:42:11 +0000

    Fix the copyright statement, the blog name is not the same as the copyright owner

--- younglives/footer.php
+++ younglives/footer.php
@@ -6,7 +6,7 @@
 							<?php bones_footer_links(); ?>
 									</nav>
 
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> Young Lives.</p>
 
 				</div> <!-- end #inner-footer -->
 
